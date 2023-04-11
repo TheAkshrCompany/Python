b892a5abb025d9c8a16806442ff6cae72410be1a
TheAkshrCompany <50172501+TheAkshrCompany@users.noreply.github.com>
2023-04-11 09:38:24 +0530

    Create backslice.py

---
+++ backslice.py
@@ -0,0 +1,4 @@
+a = ["a", "b", "c", "d", "e", "f", "g", "h"] # created the list 
+
+print(a[-1:-5:-3]) # Performing backslicing starting from position last till 5th last with step of 3
+#Output: ['h', 'e']

a990fb5d94db1440772c920181bd528b0ef09b06
TheAkshrCompany <50172501+TheAkshrCompany@users.noreply.github.com>
2023-04-11 10:35:29 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+dictionary={1:"apple",2:"banana",3:"orange"} #created dictionary using the keyword
+print(dictionary)
+dictionary1=dict(vegetalbe="lady finger", vegetalbe = "bringal",vegetalbe="potato")#created dictionary using the the contructor
+print(dictionary1)
